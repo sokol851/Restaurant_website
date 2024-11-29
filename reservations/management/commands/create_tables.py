79b4318698bbedd5e9331b85dcf9cc64602b457d
Ilia Akatov <ilia-akatoff@ya.ru>
2024-11-30 02:21:36 +0300

    Edited commands. Added model for reservations.

--- reservations/management/commands/create_tables.py
+++ reservations/management/commands/create_tables.py
@@ -1,37 +1,36 @@
-from datetime import time, date
-
+from datetime import datetime
 from django.core.management import BaseCommand
 
 from reservations.models import Table
 
 
 class Command(BaseCommand):
-
+    """
+    Создаём доступные столы
+    """
     def handle(self, *args, **options):
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        times = [
-            time(10, 00, 00),
-            time(12, 00, 00),
-            time(14, 00, 00),
-            time(16, 00, 00),
-            time(18, 00, 00),
-            time(20, 00, 00),
-            time(22, 00, 00),
+        datetime_objs = [
+            datetime(2024, 11, 29, 10),
+            datetime(2024, 11, 29, 12),
+            datetime(2024, 11, 29, 14),
+            datetime(2024, 11, 29, 16),
+            datetime(2024, 11, 29, 18),
+            datetime(2024, 11, 29, 20),
+            datetime(2024, 11, 29, 22),
         ]
-
-        date_obj = date.today()
 
         cities = [
             'SPB',
             'MSK'
         ]
+
         for city in cities:
             for number in numbers:
-                for time_obj in times:
+                for datetime_obj in datetime_objs:
                     table = Table.objects.create(
                         number=number,
-                        time=time_obj,
-                        date=date_obj,
+                        datetime=datetime_obj,
                         restaurant=city
                     )
                     table.save()
--- reservations/management/commands/update_tables.py
+++ reservations/management/commands/update_tables.py
@@ -1,19 +1,23 @@
-from datetime import date
+from datetime import timedelta, date, time, datetime
 
+import pytz
+from dateutil.relativedelta import relativedelta
 from django.core.management import BaseCommand
 
 from reservations.models import Table
 
 
 class Command(BaseCommand):
+    """
+     Находит просроченные столы и
+     обновляет дату и доступность на следующий день.
+    """
 
     def handle(self, *args, **options):
         numbers_table = Table.objects.all()
-
         for table in numbers_table:
-            table = Table.objects.filter(id=table.id).update(
-                date=date.today(), # Дата сегодня
-                # date=date(2024, 11, 30), # Или можно установить нужную дату
-                available = True,
-            )
-            print(table)
+            if datetime.now().timestamp() > table.datetime.timestamp():
+                Table.objects.filter(id=table.id).update(
+                    datetime=table.datetime + timedelta(days=1),
+                    available=True,
+                )
